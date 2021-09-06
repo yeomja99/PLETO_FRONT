f4fe113ea4e8846eaa00f4e76eea95b4732b6c7c
jungmin <kimjm0825@gmail.com>
2021-09-06 23:14:54 +0900

    LogIn, SignUp 통신 완료

--- PLECO/app/src/main/java/com/example/myapplication/communication/MasterApplication.kt
+++ PLECO/app/src/main/java/com/example/myapplication/communication/MasterApplication.kt
@@ -25,7 +25,7 @@ class MasterApplication : Application() {
             if (checkIsLogin()) {
                 getUserToken()?.let { token ->  // null이 아닌 경우 let 블럭 실행
                     val request = original.newBuilder() // 원래 나가려던 통신을 잡아서 original에 헤더를 붙임
-                        .header("Authorization", "$token")
+                        .header("X-AUTH-TOKEN", "$token")
                         // name : 서버가 설정한거 보고 다시 작성, value : 서버가 토큰 설정한거 보고 작성
                         .build()
                     it.proceed(request) // 헤더 붙이고 내보냄
@@ -36,7 +36,7 @@ class MasterApplication : Application() {
         }
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://3.35.46.179:8080/")    // 서버 배포되면 수정
+            .baseUrl("http://13.209.92.105:8080/")    // 서버 배포되면 수정
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
